87149293ac0ec0655ff6bd2651d6038f3f8e0688
Victor LH <victor.latorre@victorlh.es>
2020-12-24 10:24:16 +0100

    Fix propertie

--- src/main/java/com/victorlh/registrocontable/cloudzuulserver/security/ResourceServerConfig.java
+++ src/main/java/com/victorlh/registrocontable/cloudzuulserver/security/ResourceServerConfig.java
@@ -1,11 +1,10 @@
 package com.victorlh.registrocontable.cloudzuulserver.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -23,8 +22,8 @@ import java.util.Arrays;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	@Autowired
-	private Environment env;
+	@Value("${config.security.oauth.jwt.key}")
+	private String tokenSignKey;
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
@@ -40,14 +39,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	}
 
 	@Override
-	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+	public void configure(ResourceServerSecurityConfigurer resources) {
 		resources.tokenStore(tokenStore());
 	}
 
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		jwtAccessTokenConverter.setSigningKey(env.getProperty("config.security.ouath.jwt.key"));
+		jwtAccessTokenConverter.setSigningKey(tokenSignKey);
 		return jwtAccessTokenConverter;
 	}
 
@@ -73,7 +72,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public FilterRegistrationBean<CorsFilter> corsFilter() {
 		CorsConfigurationSource source = corsConfigurationSource();
 		CorsFilter corsFilter = new CorsFilter(source);
-		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(corsFilter);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(corsFilter);
 		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		return bean;
 	}
